626e2e41a550fe1c187d4f44acab65a1b3a87a22
Markel Cuesta <cuestaarribas.markel@proton.me>
2022-06-09 13:36:22 +0200

    project pager working

--- app/src/components/ProjectPaginator.vue
+++ app/src/components/ProjectPaginator.vue
@@ -4,18 +4,17 @@ import { onMounted } from 'vue'
 const props = defineProps(['page', 'repos'])
 const emit = defineEmits(['update:page', 'update:repos'])
     
-const getRepos = (e) => {
-    emit('update:page', props.page + 1)
+const getRepos = (newPage) => {
+    emit('update:page', newPage)
     axios
-        .get(`https://api.github.com/users/markelca/repos?per_page=${6}&page=${props.page}`)
+        .get(`https://api.github.com/users/markelca/repos?per_page=${6}&page=${newPage}`)
         .then(response => {
             emit('update:repos', response.data)
-            console.log(response.data)
         })
         .catch((error) => console.log(error))
 }
 onMounted(() => {
-  getRepos()
+    getRepos(props.page)
 })
 </script>
 
@@ -24,7 +23,7 @@ onMounted(() => {
 <div class="flex justify-center">
   <nav aria-label="Page navigation example">
     <ul class="flex list-style-none">
-      <li class="page-item disabled" @click='$emit("update:page", --page)'><a
+      <li class="page-item disabled" @click='getRepos(page - 1)'><a
           class="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-500 pointer-events-none focus:shadow-none"
           href="#" tabindex="-1" aria-disabled="true">Previous</a></li>
       <li class="page-item"><a
@@ -36,7 +35,7 @@ onMounted(() => {
       <li class="page-item"><a
           class="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
           href="#">3</a></li>
-      <li class="page-item" @click='getRepos'><a
+      <li class="page-item" @click='getRepos(page + 1)'><a
           class="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none">Next</a></li>
     </ul>
   </nav>
