2db9a613ffeb6f2cc7e45ff15d07df5ef7ca5c06
Markel Cuesta <cuestaarribas.markel@proton.me>
2022-06-09 13:32:16 +0200

    project pager working

--- app/src/components/ProjectList.vue
+++ app/src/components/ProjectList.vue
@@ -1,32 +1,16 @@
 <script setup>
 import ProjectCard from './ProjectCard.vue'
-import axios from 'axios'
 import { ref, onMounted } from 'vue'
 import ProjectPaginator from './ProjectPaginator.vue'
 
 const repos = ref([])
 const page = ref(1)
 
-const getRepos = (e) => {
-    page.value++
-    axios
-        .get(`https://api.github.com/users/markelca/repos?per_page=${6}&page=${page.value}`)
-        .then(response => {
-            repos.value = response.data
-            console.log(response.data)
-        })
-        .catch((error) => console.log(error))
-}
-onMounted(() => {
-  getRepos()
-})
 </script>
 <template>
-    <button @click='getRepos'>More</button>
     {{page}}
-    <ProjectPaginator v-model='page'/>
+    <ProjectPaginator v-model:page='page' v-model:repos='repos'/>
     <div id='project-list' class='grid grid-cols-1 sm:grid-cols-3'>
         <ProjectCard v-for='repo in repos' :name='repo.name' :description='repo.description'/>
     </div>
-    <ProjectPaginator/>
 </template>
--- app/src/components/ProjectPaginator.vue
+++ app/src/components/ProjectPaginator.vue
@@ -1,12 +1,30 @@
 <script setup>
-    defineProps(['modelValue'])
-    defineEmits(['update:modelValue'])
+import axios from 'axios'
+import { onMounted } from 'vue'
+const props = defineProps(['page', 'repos'])
+const emit = defineEmits(['update:page', 'update:repos'])
+    
+const getRepos = (e) => {
+    emit('update:page', props.page + 1)
+    axios
+        .get(`https://api.github.com/users/markelca/repos?per_page=${6}&page=${props.page}`)
+        .then(response => {
+            emit('update:repos', response.data)
+            console.log(response.data)
+        })
+        .catch((error) => console.log(error))
+}
+onMounted(() => {
+  getRepos()
+})
 </script>
+
 <template>
+    <button @click='getRepos'>More</button>
 <div class="flex justify-center">
   <nav aria-label="Page navigation example">
     <ul class="flex list-style-none">
-      <li class="page-item disabled"><a
+      <li class="page-item disabled" @click='$emit("update:page", --page)'><a
           class="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-500 pointer-events-none focus:shadow-none"
           href="#" tabindex="-1" aria-disabled="true">Previous</a></li>
       <li class="page-item"><a
@@ -18,7 +36,7 @@
       <li class="page-item"><a
           class="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
           href="#">3</a></li>
-      <li class="page-item" @click='$emit("update:modelValue", modelValue++)'><a
+      <li class="page-item" @click='getRepos'><a
           class="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none">Next</a></li>
     </ul>
   </nav>
