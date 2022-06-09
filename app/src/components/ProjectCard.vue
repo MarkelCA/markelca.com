2c5c1d831d2a97657153b3693771d8f4238a9dcf
Markel Cuesta <cuestaarribas.markel@proton.me>
2022-06-09 10:29:44 +0200

    fetching repos from github api

--- app/src/components/ProjectCard.vue
+++ app/src/components/ProjectCard.vue
@@ -1,15 +1,15 @@
+<script setup>
+    defineProps(['name', 'description'])
+</script>
 <template>
 <div class="flex justify-center">
   <div class="rounded-lg shadow-lg bg-white max-w-sm transition duration-500 ease-in-out transform hover:-translate-y-4 hover:shadow-2xl">
     <a href="#!">
-      <img class="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt=""/>
+      <img class="rounded-t-lg w-full" src="https://picsum.photos/400/300" alt=""/>
     </a>
     <div class="p-6">
-      <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
-      <p class="text-gray-700 text-base mb-4">
-        Some quick example text to build on the card title and make up the bulk of the card's
-        content.
-      </p>
+      <h5 class="text-gray-900 text-xl font-medium mb-2">{{name}}</h5>
+      <p class="text-gray-700 text-base mb-4">{{description}}</p>
       <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
     </div>
   </div>
--- app/src/components/ProjectList.vue
+++ app/src/components/ProjectList.vue
@@ -1,10 +1,23 @@
 <script setup>
 import ProjectCard from './ProjectCard.vue'
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+
+// declare a ref to hold the element reference
+// the name must match template ref value
+const repos = ref([])
+
+onMounted(() => {
+    axios
+        .get('https://api.github.com/users/markelca/repos')
+        .then(response => {
+            repos.value = response.data
+            console.log(repos)
+        })
+})
 </script>
 <template>
-    <div id='project-list' class='flex space-x-8'>
-        <ProjectCard/>
-        <ProjectCard/>
-        <ProjectCard/>
+    <div id='project-list' class='flex flex-wrap space-x-2 md:space-x-8 sm:space-y-6 md:space-y-0 flex-col md:flex-row lg-flex-row'>
+        <ProjectCard v-for='repo in repos' :name='repo.name' :description='repo.description'/>
     </div>
 </template>
